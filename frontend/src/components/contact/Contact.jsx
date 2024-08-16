bea9100f1c0226b24cf1dee5addc544daf6a699b
zeynepucuncuoglu <zeynepucuncuogluu@gmail.com>
2024-08-16 14:26:12 +0300

    developers added

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import zeynepCodefolio from '../../assets/zeynepCodefolio.jpg';
+import boraCodefolio from '../../assets/boraCodefolio.jpg';
+import boranCodefolio from '../../assets/boranCodefolio.jpeg';
+import torkanCodefolio from '../../assets/torkanCodefolio.jpeg';
+import mehmetCodefolio from '../../assets/mehmetCodefolio.jpg';
+import linkedin from '../../assets/linkedin.png';
+const developers = [
+  {
+    name: 'Bora Fenari Köstem',
+    photo: boraCodefolio,
+    linkedin: 'https://www.linkedin.com/in/bora-fenari-köstem-7353681a3/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+    email: 'fenari.kostem@gmail.com',
+  },
+  {
+    name: 'Torkan Hesari',
+    photo: torkanCodefolio,
+    linkedin: 'https://www.linkedin.com/in/torkanhesari/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    email: 'torkanhesari78@gmail.com',
+  },
+  {
+    name: 'Boran Kılıçaslan',
+    photo: boranCodefolio,
+    linkedin: 'https://www.linkedin.com/in/borankilicaslan/',
+    email: 'boran.kilicaslan@hotmail.com',
+  },
+  {
+    name: 'Mehmet Akif Yavuz',
+    photo: mehmetCodefolio,
+    linkedin: 'https://www.linkedin.com/in/mehmet-akif-yavuz-91256a246/',
+    email: 'akif.yavuz@ug.bilkent.edu.tr',
+  },
+  {
+    name: 'Zeynep Göksu Üçüncüoğlu',
+    photo: zeynepCodefolio,
+    linkedin: 'https://www.linkedin.com/in/zeynepucuncuoglu/',
+    email: 'zeynepucuncuogluu@gmail.com',
+  }
+];
 
 const Contact = () => {
   return (
@@ -24,7 +61,23 @@ const Contact = () => {
           <FontAwesomeIcon icon={faPhone} />
         </a>
       </div>
-      
+      <h2 className="text-3xl font-semibold mb-6 codefolio-yellow">Meet Our Developers</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {developers.map((dev, index) => (
+          <div key={index} className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center">
+            <img src={dev.photo} alt={`${dev.name} photo`} className="w-32 h-32 rounded-full mb-4" />
+            <h2 className="text-2xl font-bold mb-2">{dev.name}</h2>
+            <div className="flex space-x-4">
+              <a href={dev.linkedin} target="_blank" rel="noopener noreferrer">
+              <img src={linkedin} alt="linkedin" style={{ width: '24px', height: '24px'}} />
+              </a>
+              <a href={`mailto:${dev.email}`}>
+                <FontAwesomeIcon icon={faEnvelope} className="text-2xl codefolio-yellow" />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
